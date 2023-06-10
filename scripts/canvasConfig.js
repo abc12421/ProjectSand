ddce7690a129474cb74ac2b8e10bb95c38445b53
Abc 12421 <77096076+abc12421@users.noreply.github.com>
2023-06-10 16:04:48 +0530

    Update canvasConfig.js

--- scripts/canvasConfig.js
+++ scripts/canvasConfig.js
@@ -20,9 +20,8 @@
 const __max_width = 560;
 const __max_height = 480;
 
-/* Need to use a smaller size when on mobile devices with small screens */
-const width = Math.min(__max_width, Math.max(screen.width - 6, 1));
-const height = Math.min(__max_height, Math.max(screen.height - 200, 100));
+const width =  Math.trunc(visualViewport.width-1);
+const height = width > visualViewport.height ? Math.trunc(visualViewport.height) :  Math.trunc(visualViewport.height-200);
 
 const MAX_FPS = 120;
 const DEFAULT_FPS = 60;
